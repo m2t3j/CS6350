da85bb64b8010f789a914ec4acdcc46c188dcb91
Matt Jensen <mattjensen625@gmail.com>
2024-10-19 19:23:46 -0600

    run.sh file

--- Ensemble Learning/run.sh
+++ Ensemble Learning/run.sh
@@ -8,5 +8,7 @@ python "Ensemble Learning/2b.py"
 echo "2c: Yes, Random Forests generally perform better than Bagged Trees because the added randomness in feature selection reduces overfitting and improves generalization."
 echo "However, 2c takes over 3 hours to run, and is very computationally inefficient, even with parallelism, so I decided not to include it in the .sh as it is a waste of your time and mine to wait that long."
 
+echo "2d: Takes too long to run, did not do"
+
 echo "Running 4.py: Linear Regression Task..."
 python "Linear Regression/4.py"

8f6f7935d13cefc5678ae8f7ade9dab3e1b64670
Matt Jensen <mattjensen625@gmail.com>
2024-11-26 21:19:08 -0700

    updated run.sh

--- SVM/run.sh
+++ SVM/run.sh
@@ -1,8 +1,8 @@
 echo "Running 2.py..."
-python 2.py
+python SVM/2.py
 
-echo "Running 3.py..."
-python 3.py
+echo "Running 3.py...(take about 10 minutes)"
+python SVM/3.py
 
-echo "Running 3bc.py..."
-python 3bc.py
+echo "Running 3bc.py...(takes about 35 minutes per combination. About 8 hrs in total to run)"
+python SVM/3bc.py
